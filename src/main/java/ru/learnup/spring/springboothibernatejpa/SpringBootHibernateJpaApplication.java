50c7eb2414baa37f86c3cc40eafe0b68b4ce3d7c
aln7ex <alexmisak@yandex.ru>
2022-04-14 12:35:28 +0300

    Lesson 27 Version In Process

--- src/main/java/ru/learnup/spring/springboothibernatejpa/SpringBootHibernateJpaApplication.java
+++ src/main/java/ru/learnup/spring/springboothibernatejpa/SpringBootHibernateJpaApplication.java
@@ -4,17 +4,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.slf4j.*;
+import ru.learnup.spring.springboothibernatejpa.entity.Product;
+import ru.learnup.spring.springboothibernatejpa.service.AuthorService;
 import ru.learnup.spring.springboothibernatejpa.service.BookService;
+import ru.learnup.spring.springboothibernatejpa.service.ProductService;
 
 @SpringBootApplication
 public class SpringBootHibernateJpaApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SpringBootHibernateJpaApplication.class);
+    static void updateAsync(ProductService service) {
+
+        Product product = service.getProductById(3);
+        product.setName("new product");
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> service.update(product)).start();
+        }
+    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringBootHibernateJpaApplication.class, args);
         BookService bookService = context.getBean(BookService.class);
+        AuthorService authorService = context.getBean(AuthorService.class);
 
-        log.info("All books by author_id {}: {}", 2, bookService.customFindAllBookByAuthorId(2));
+        ProductService productService = context.getBean(ProductService.class);
+
+//        log.info("Book {} ", authorService.getAuthorById(1));
+        log.info("{}", productService.getProductById(3));
+//        updateAsync(productService);
+
+
+//        log.info("All books by author_id {}: {}", 2, bookService.customFindAllBookByAuthorId(2));
     }
 }
---
+++ src/main/java/ru/learnup/spring/springboothibernatejpa/entity/Product.java
@@ -0,0 +1,31 @@
+package ru.learnup.spring.springboothibernatejpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "book_shop")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Version
+    @Valid
+    @NotBlank
+    private Integer version;
+}
